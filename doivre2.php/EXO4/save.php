d9f48ea085ecf97bef34c7da31ba84fcaf6714aa
khalidtariq <62527327+khalidtariq@users.noreply.github.com>
2020-04-04 02:08:38 +0100

    Update save.php

--- doivre2.php/EXO4/save.php
+++ doivre2.php/EXO4/save.php
@@ -1,6 +1,6 @@
 /*TARIQ JARRARI && KHALID BEN TAGUAR*/
 <?php
-function valider_email(string $va){
+function valider_email(string $va){/* fonction qui retourne un TRUE si le login est une adresse mail valide ( sous la forme tata@cocomail.com )*/
 	
 	if (preg_match("#^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$#i", $va))  
 
@@ -9,7 +9,7 @@ return true;
 }
 
 
-function valider_pass(string $var){
+function valider_pass(string $var){/* une fonction qui retourne un TRUE  si le mot de passe est valide*/
 	
 if (preg_match('#^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*\W)#', $var) or strlen($var)>=8) 
 
@@ -21,27 +21,29 @@ return true;
 
 <?php
 
-
+/* un script php qui permet de vérifier d’abord si   les deux fonctions function valider_pass(string $var) et function valider_email(string $va) ont retournées TRUE, ensuite il vérifie si une adresse et un mot de passe dans le fichier resultats.txt*/
 
 if(valider_email($_POST["login"]) and valider_pass($_POST["pass"] )){
-	$fd=fopen("resultats.txt","r");
-while (!feof($fd)) {
-$ligne=fgets($fd,255);
-if(!empty($ligne)) {
-$reponses=explode("|",$ligne);
-if($_POST["login"]==$reponses[0]) echo "<script>alert(\"Authentification réussie \")</script>";
+    $fd=fopen("resultats.txt","r");
+       while (!feof($fd)) {
+      $ligne=fgets($fd,255);
+       if(!empty($ligne)) {
+     $reponses=explode("|",$ligne);
+      if($_POST["login"]==$reponses[0]) echo "<script>alert(\"Authentification réussie \")</script>";
 
-}
-}
-if($_POST["login"]!=$reponses[0]){
- $fd=fopen("resultats.txt","a+");
+                           }
+                          }/* Fin while*/
+if($_POST["login"]!=$reponses[0]){/* Dans un fichier resultats.txt est stocké une liste nouvelle login ( adresses mail) et mots de passe.*/
+
+ $fd=fopen("resultats.txt","a");
 fputs($fd,$_POST["login"]);
 fputs($fd,"|");
 fputs($fd,$_POST["pass"]);
 fputs($fd,"\n");
  echo "<script>alert(\"bien enregistrés \")</script>";
-}
-fclose($fd);}
+                                  }/* Fin if*/
+                                                         fclose($fd);}/* Fin if*/
+
 else echo "<script>alert(\"email incorrecte ou Mot de passe invalid \")</script>";
 ?>
 
